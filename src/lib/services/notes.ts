1b210e958204b31baa720a5c2e405bccdbbd2d9b
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2023-10-21 22:43:05 -0600

    get ready for save logic

--- src/lib/services/notes.ts
+++ src/lib/services/notes.ts
@@ -2,11 +2,14 @@ import { get } from 'svelte/store';
 import { Template } from '../../db/types.js';
 import * as State from '../stores/template.js'
 import { dbService } from '../../db/service.js';
+import { appState } from '../stores/appState.js';
 
 class NoteService {
     async handleSave(event: KeyboardEvent){
         if ((event.metaKey || event.ctrlKey) && event.key === 's') {
-            event.preventDefault(); // Prevent the default browser action (saving the page)
+            const state = get(appState)
+            console.log('state', state);
+            
             
           }
     }
