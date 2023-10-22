8168474936d13b87f565bffb42f4a544b59e7be6
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2023-10-21 20:15:29 -0600

    apply db schemas if they are not there

--- src/db/index.ts
+++ src/db/index.ts
@@ -14,17 +14,38 @@ export class DB {
         return (await this.db).execute(query, bindValues ? bindValues : undefined)
     }
    private async applyShemas(db: Promise<Database>){
-       console.log('applying schemas');
+      const createTemplates =  (await (await db).execute(`CREATE TABLE IF NOT EXISTS templates (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            title TEXT NOT NULL,
+            content TEXT,
+            tag TEXT default 'default'
+        );`))
+
+        const notes = (await (await db).execute(`CREATE TABLE IF NOT EXISTS notes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updated_at DATERIME DEFAULT CURRENT_TIMESTAMP,
+                    description TEXT,
+                    phone INTEGER,
+                    asset TEXT,
+                    machine TEXT
+                    );`))
+        
+        if (createTemplates.rowsAffected >= 1 && notes.rowsAffected >= 1) {
+            (await db).execute(`INSERT INTO templates (title, content) VALUES (
+                'General Call Template',
+                'User''s Stated Issue:
+            When did it last work:
+            How many users are affected: 
+            
+            Troubleshooting Steps:
+            -------------------------------'
+            );`)
+        }
        
         
     }
 }
 
-const schemas = {
-    "notes": `CREATE TABLE IF NOT EXISTS notes (
-        id INTEGER
-        description TEXT,
-        phone INTEGER,
-        asset TEXT,
-        machine TEXT);`
-}
