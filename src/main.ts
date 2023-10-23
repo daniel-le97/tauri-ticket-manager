e83da4a02c118fed0bbca2969875b4bfa8a6d160
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2023-10-23 15:09:08 -0600

    clean imports

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import "./styles.css";
 import App from "./App.svelte";
 import { db } from "./db/index.js";
-import { noteService } from "./lib/services/notes.js";
-import { get } from "svelte/store";
-import { appState } from "./lib/stores/appState.js";
+
 let init = db
 
 
