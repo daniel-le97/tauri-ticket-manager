deae784c12ac3f3b70e0a2531fccccb6753cdde2
tung le <tung.le0319@gmail.com>
2023-10-24 16:54:54 -0600

    ticket history

--- src/lib/Components/TicketHistory.svelte
+++ src/lib/Components/TicketHistory.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
-  import {
-    NoteDTO,
-  } from "../../db/types.js";
+  import { NoteDTO } from "../../db/types.js";
   import { dbService } from "../../db/service.js";
   import { onMount } from "svelte";
   import { getDate } from "../utils/date.js";
-
+  import { resetAppState } from "../stores/appState.js";
+  import {
+    Table,
+    TableBody,
+    TableBodyCell,
+    TableBodyRow,
+    TableHead,
+    TableHeadCell,
+  } from "flowbite-svelte";
   let tickets: NoteDTO[];
 
   onMount(async () => {
@@ -15,41 +21,44 @@
   async function getTickets() {
     try {
       tickets = await dbService.notes.getAll();
-      console.log(tickets);
-      
-    } catch (error) {
-      
-    }
+      // console.log(tickets);
+    } catch (error) {}
+  }
+
+  function handleClick(ticket: NoteDTO) {
+    resetAppState(ticket);
   }
 </script>
 
 {#if tickets}
-<table>
-  <thead>
-    <tr>
-      <th>Ticket ID</th>
-      <th>Asset</th>
-      <th class="" on:click={() => console.log('hello world')}>Created At</th>
-      <th>Email</th>
-      <th>Phone</th>
-      <th>Last updated</th>
-      <th>Description</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#each tickets as item (item.id)}
+  <table class="">
+    <thead>
       <tr>
-        <td>{item.id}</td>
-        <td>{item.asset}</td>
-        <td>{getDate(item.created_at)}</td>
-        <td>{item.email}</td>
-        <td>{item.phone}</td>
-        <td>{getDate(item.updated_at)}</td>
-        <td>{item.description}</td>
+        <th>Ticket ID</th>
+        <th>Asset</th>
+        <th class="" on:click="{() => console.log('hello world')}"
+          >Created At</th
+        >
+        <th>Email</th>
+        <th>Phone</th>
+        <th>Last updated</th>
+        <th>Description</th>
       </tr>
-    {/each}
-  </tbody>
-</table>
+    </thead>
+    <tbody>
+      {#each tickets as item (item.id)}
+        <tr on:click="{() => handleClick(item)}">
+          <td>{item.id}</td>
+          <td>{item.asset}</td>
+          <td>{getDate(item.created_at)}</td>
+          <td>{item.email}</td>
+          <td>{item.phone}</td>
+          <td>{getDate(item.updated_at)}</td>
+          <td class="truncate overflow-x-clip">{item.description}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
 {/if}
 
 <style>
@@ -58,7 +67,8 @@
     width: 100%;
   }
 
-  th, td {
+  th,
+  td {
     border: 1px solid #ccc;
     padding: 8px;
   }
@@ -70,5 +80,36 @@
 
   th:hover {
     background-color: #f0f0f0; /* Change background on hover to indicate interactivity */
+  }
+  .auto-width-table {
+    table-layout: auto;
+    border-collapse: collapse;
+  }
+
+  .auto-width-table td {
+    width: auto;
+    padding: 5px;
+    border: 1px solid #000;
+  }
+
+  td {
+    width: fit-content !important;
+  }
+
+  .truncate {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .overflow-x-clip {
+    max-width: 100px; /* Adjust this value to control the width before text truncation occurs */
+  }
+
+  .fit-content-and-truncate {
+    max-width: 0;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
   }
 </style>
